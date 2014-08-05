319ed901390372fad924c53ba8a5a23e44517f75
Joshua Moody <joshimoo@hotmail.de>
2014-08-05 17:28:45 +0200

    Added Simple Insertion Sort for numbers Added Test for Simple Insertion Sort for numbers

---
+++ JamLib/Algorithms/Sorting/InsertionSorts.cs
@@ -0,0 +1,43 @@
+﻿namespace JamLib.Algorithms.Sorting
+{
+
+    /// <summary>
+    /// Wikipedia: http://en.wikipedia.org/wiki/Insertion_sort
+    /// Insertion sort 
+    /// Shell sort
+    /// Splay sort
+    /// Tree sort
+    /// Library sort
+    /// Patience sorting
+    /// </summary>
+    public static class InsertionSorts
+    {
+        // I Like the IComparer approach much more, since it is generic.
+        // But since this is mearly a toy it does not really matter.
+        // TODO: Think about Extension Method vs Regular Method 
+        // Pseudo Code:
+        // Increase Search Space
+        // Traverse Search Space in Reverse
+        // Swap Element if smaller
+
+        public static int[] InsertionSort(this int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i; j > 0; j--)
+                {
+                    // Swap if smaller
+                    if (array[j] < array[j - 1])
+                    {
+                        // Could be done in place
+                        int temp = array[j - 1];
+                        array[j - 1] = array[j];
+                        array[j] = temp;
+                    }
+                    else { break; } // TODO: Check in the for loop exit condition, since we don't need to check the rest of the array if this fails. Since they are already sorted
+                }
+            }
+            return array;
+        }
+    }
+}
---
+++ JamLibTests/Algorithms/Sorting/InsertionSortsTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JamLib.Algorithms.Sorting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace JamLib.Algorithms.Sorting.Tests
+{
+    [TestClass()]
+    public class InsertionSortsTests
+    {
+        [TestMethod()]
+        public void InsertionSortTest()
+        {
+            // NOTE: Using InsertionSort() directly instead of as an extension method since, it's more expressive.
+            int[] actual = new int[] { 12, 10, 4, 5, 0, 6, 2, 1, -4, -24 };
+            int[] expected = new int[] { -24, -4, 0, 1, 2, 4, 5, 6, 10, 12 };
+            int[] result = InsertionSorts.InsertionSort(actual);
+
+            CollectionAssert.AreEqual(expected, result, "InsertionSort did not sort correctly");
+            // CollectionAssert.AreEqual
+            // Enumerable.SequenceEqual 
+        }
+    }
+}
