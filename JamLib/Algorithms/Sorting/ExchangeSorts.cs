8f8e19b279f6cf2be7cb476ab5f19eb2ae1a2ddd
Joshua Moody <joshimoo@hotmail.de>
2014-08-06 02:59:00 +0200

    Added BubbleSort

---
+++ JamLib/Algorithms/Sorting/ExchangeSorts.cs
@@ -0,0 +1,30 @@
+﻿namespace JamLib.Algorithms.Sorting
+{
+    public static class ExchangeSorts
+    {
+        public static int[] BubbleSort(this int[] array)
+        {
+            // Run this atleast once, could use a do while instead
+            bool swapped = true;
+            while (swapped)
+            {
+                // Reset Swap Status
+                swapped = false;
+
+                for (int i = 1; i < array.Length; i++)
+                {
+                    // if this pair is out of order, swap them and remember something changed
+                    if (array[i - 1] > array[i])
+                    {
+                        int temp = array[i - 1];
+                        array[i - 1] = array[i];
+                        array[i] = temp;
+                        swapped = true;
+                    }
+                }
+            }
+            return array;
+        }
+
+    }
+}
---
+++ JamLibTests/Algorithms/Sorting/ExchangeSortsTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JamLib.Algorithms.Sorting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace JamLib.Algorithms.Sorting.Tests
+{
+    [TestClass()]
+    public class ExchangeSortsTests
+    {
+        [TestMethod()]
+        public void BubbleSortTest()
+        {
+            int[] actual = new int[] { 12, 10, 4, 5, 0, 6, 2, 1, -4, -24 };
+            int[] expected = new int[] { -24, -4, 0, 1, 2, 4, 5, 6, 10, 12 };
+            int[] result = ExchangeSorts.BubbleSort(actual);
+
+            CollectionAssert.AreEqual(expected, result, "BubbleSort did not sort correctly");
+        }
+    }
+}
