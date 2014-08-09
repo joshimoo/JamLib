b4ed8bb57abd826ba4a35f23f0edbc1f1ec1e1f6
Joshua Moody <joshimoo@hotmail.de>
2014-08-09 23:49:55 +0200

    Added a generic Swap Method

---
+++ JamLib/Algorithms/Sorting/SortingUtils.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JamLib.Algorithms.Sorting
+{
+    public static class SortingUtils
+    {
+        public static void Swap<T>(IList<T> data, int i, int j)
+        {
+            T temp = data[i];
+            data[i] = data[j];
+            data[j] = temp;
+        }
+    }
+}
