7cc87fa73e3c69d421c015f4b8ec9cb445dc5eb7
Joshua Moody <joshimoo@hotmail.de>
2014-08-15 01:06:41 +0200

    Implemented CombSort which is a BubbleSort with different gap sizes, have a look at ShellSort for a similar idea for InsertionSort

---
+++ JamLib/Algorithms/Sorting/ExchangeSorts/CombSort.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+
+namespace JamLib.Algorithms.Sorting.ExchangeSorts
+{
+    public static class CombSort
+    {
+        // TODO: Think about adding additional overloads for IComparable as well as Comparison (so that we can use lambda expressions)
+        public static void Sort<T>(IList<T> data) { Sort(data, Comparer<T>.Default); }
+        public static void Sort<T>(IList<T> data, IComparer<T> comparer)
+        {
+            int gap = data.Count; //initialize gap size
+            float shrink = 1.3f; //set the gap shrink factor
+            bool sorted = false;
+
+            while (gap > 1 || !sorted)
+            {
+                // Update Gap size
+                gap = (int)(gap / shrink);
+                if (gap < 1) { gap = 1; }
+
+                // Reset Sort Status
+                sorted = true;
+
+                for (int i = 0; i + gap < data.Count; i++)
+                {
+                    // if this pair is out of order, swap them and remember something changed
+                    if (comparer.Compare(data[i], data[i + gap]) > 0)
+                    {
+                        SortingUtils.Swap(data, i, i + gap);
+                        sorted = false;
+                    }
+                }
+
+            }
+        }
+
+
+    }
+}
--- JamLib/Algorithms/Sorting/ExchangeSorts/README.md
+++ JamLib/Algorithms/Sorting/ExchangeSorts/README.md
@@ -1,7 +1,7 @@
 ﻿Bubble sort: http://en.wikipedia.org/wiki/Bubble_sort
 Cocktail sort: http://en.wikipedia.org/wiki/Cocktail_sort
 Odd–even sort: http://en.wikipedia.org/wiki/Odd–even_sort
-Comb sort:
+Comb sort: http://en.wikipedia.org/wiki/Comb_sort
 Gnome sort:
 Quicksort: http://en.wikipedia.org/wiki/Quicksort
 Stooge sort:
