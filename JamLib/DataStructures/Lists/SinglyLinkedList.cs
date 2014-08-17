0eb8647844114995e5532c3c3e5cfa354606b44c
Joshua Moody <joshimoo@hotmail.de>
2014-08-17 18:32:51 +0200

    Implemented a SinglyLinkedList<T> that implements the ICollection<T> Interface closes #6 The add method, justs adds the items always in front of the head. Still need to implement an enumerator.

---
+++ JamLib/DataStructures/Lists/SinglyLinkedList.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace JamLib.DataStructures.Lists
+{
+    /// <summary>
+    /// NOTE: DO NOT USE THIS CLASS, INSTEAD USE THE NET FRAMEWORK IMPLEMENTATION
+    /// Since this is just for a toy project it's missing quite a bit of error checking and boilerplate code
+    /// </summary>
+    public class SinglyLinkedList<T> : ICollection<T>
+    {
+        private int count;
+        public int Count { get { return count; } }
+        public bool IsReadOnly { get { return false; } }
+
+        private SinglyLinkedListNode<T> head;
+        public SinglyLinkedListNode<T> First { get { return head; } }
+
+        public void Add(T value)
+        {
+            // NOTE: This is not needed since when the head is null, we are passing null anyway. Leaving it for the moment, till I refactor to use a tail instead of adding as a new head.
+            if (head == null)
+            {
+                // First Element
+                var node = new SinglyLinkedListNode<T>(value, null);
+                head = node;
+            }
+            else
+            {
+                // Add the new node as the head, and link to the old head
+                var node = new SinglyLinkedListNode<T>(value, head);
+                head = node;
+            }
+
+            count++;
+        }
+
+        // This is dumb since it uses O(n), much better would be a DoublyLinkedList since then I could use the prev pointer from the current node.
+        public bool Remove(T item)
+        {
+            var currNode = head;
+            var prevNode = currNode;
+            while (currNode != null)
+            {
+                if (Equals(currNode.Value, item))
+                {
+                    // Hook this up
+                    prevNode.Next = currNode.Next;
+                    count--;
+                    return true;
+                }
+
+                prevNode = currNode;
+                currNode = currNode.Next;
+            }
+
+            return false;
+        }
+
+        public void Clear()
+        {
+            head = null;
+            count = 0;
+        }
+
+        public bool Contains(T item) { return Find(item) != null; }
+        public SinglyLinkedListNode<T> Find(T value)
+        {
+            var node = head;
+            while (node != null)
+            {
+                if (Equals(node.Value, value)) { return node; }
+                node = node.Next;
+            }
+
+            return null;
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            // There is not enough Space in the Array to hold all list items
+            if (arrayIndex < 0 || (array.Length - arrayIndex) < count) { throw new ArgumentOutOfRangeException(); }
+
+            var node = head;
+            while (node != null)
+            {
+                array[arrayIndex++] = node.Value;
+                node = node.Next;
+            }
+        }
+
+
+        // TODO: Implement Enumerator
+        public IEnumerator<T> GetEnumerator() { throw new System.NotImplementedException(); }
+        IEnumerator IEnumerable.GetEnumerator() { return GetEnumerator(); }
+
+        public class SinglyLinkedListNode<T>
+        {
+            public SinglyLinkedListNode<T> Next { get; set; }
+            public T Value { get; set; }
+
+            public SinglyLinkedListNode(T value, SinglyLinkedListNode<T> next)
+            {
+                Value = value;
+                Next = next;
+            }
+        }
+    }
+}
---
+++ JamLibTests/DataStructures/Lists/SinglyLinkedListTests.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using JamLib.DataStructures.Lists;
+
+namespace JamLib.DataStructures.Lists.Tests
+{
+    [TestClass()]
+    public class SinglyLinkedListTests
+    {
+        [TestMethod()]
+        public void AddTest()
+        {
+            var list = new SinglyLinkedList<int> { 1, 0, -1 };
+            Assert.AreEqual(3, list.Count, "Did not add the items");
+            list.Add(1);
+            Assert.AreEqual(4, list.Count, "Did not add the item");
+        }
+
+        [TestMethod()]
+        public void RemoveTest()
+        {
+            var list = new SinglyLinkedList<int> { 1, 0, -1, 1 };
+            Assert.IsTrue(list.Remove(1));
+            Assert.AreEqual(3, list.Count, "Did not remove the item");
+        }
+
+        [TestMethod()]
+        public void ClearTest()
+        {
+            var list = new SinglyLinkedList<int> { 1, 0, -1 };
+            list.Clear();
+            Assert.AreEqual(0, list.Count, "List Size does not match");
+        }
+
+        [TestMethod()]
+        public void ContainsTest()
+        {
+            var list = new SinglyLinkedList<int> { 1, 0, -1 };
+            Assert.IsTrue(list.Contains(0), "The list does not contain the item");
+        }
+
+        [TestMethod()]
+        public void CopyToTest()
+        {
+            // NOTE: the Add method, adds them to the front of the list, therefore the list is in reverse
+            var list = new SinglyLinkedList<int> { 1, 0, -1 };
+            var expected = new int[] { -1, 0, 1 };
+            var actual = new int[list.Count];
+            list.CopyTo(actual, 0);
+
+            CollectionAssert.AreEqual(expected, actual, "The arrays do not match");
+        }
+    }
+}
