1c786f77a67e34f93b99051a7c921c55b51f697a
Joshua Moody <joshimoo@hotmail.de>
2014-08-20 23:05:49 +0200

    Implemented a LinearSearch for Ilist<T> data A custom comparer can be provided incase one is only interested in an item with a specific property the default comparer does equality comparison.

---
+++ JamLib/Algorithms/Searching/LinearSearch.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JamLib.Algorithms.Searching
+{
+    public static class LinearSearch
+    {
+        public static int Search<T>(this IList<T> data, T element) { return Search(data, element, Comparer<T>.Default); }
+        public static int Search<T>(this IList<T> data, T element, IComparer<T> comparer)
+        {
+            for (int i = 0; i < data.Count; i++)
+            {
+                if (comparer.Compare(data[i], element) == 0) { return i; }
+            }
+
+            return -1;
+        }
+
+        public static int ReverseSearch<T>(this IList<T> data, T element) { return ReverseSearch(data, element, Comparer<T>.Default); }
+        public static int ReverseSearch<T>(this IList<T> data, T element, IComparer<T> comparer)
+        {
+            for (int i = data.Count - 1; i >= 0; i--)
+            {
+                if (comparer.Compare(data[i], element) == 0) { return i; }
+            }
+
+            return -1;
+        }
+
+    }
+}
---
+++ JamLibTests/Algorithms/Searching/LinearSearchTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace JamLib.Algorithms.Searching.Tests
+{
+    [TestClass()]
+    public class LinearSearchTests
+    {
+        [TestMethod()]
+        public void SearchTest()
+        {
+            var data = new int[] { -1, 0, 1, 4, 5, 7, 88, 90, 111, 160, 250 };
+            int actualIndex = LinearSearch.Search(data, 90);
+            int expectedIndex = 7;
+
+            Assert.AreEqual(expectedIndex, actualIndex, "Linear search did not provide the correct index");
+        }
+
+        [TestMethod()]
+        public void ReverseSearchTest()
+        {
+            var data = new int[] { -1, 0, 1, 4, 5, 7, 88, 90, 111, 160, 250 };
+            int actualIndex = LinearSearch.ReverseSearch(data, 90);
+            int expectedIndex = 7;
+
+            Assert.AreEqual(expectedIndex, actualIndex, "Reverse Linear search did not provide the correct index");
+        }
+
+        [TestMethod()]
+        public void Search_CustomComparer_Test()
+        {
+            // This is a rather stupid example for a custom comparer
+            var data = new Person[] { new Person() { Age = 2, Name = "firstname" }, new Person() { Age = 66, Name = "middlename" }, new Person() { Age = 33, Name = "lastname" } };
+
+            Comparison<Person> stupidExample = ((x, y) => x.Age > y.Age ? 1 : x.Age < y.Age ? -1 : 0);
+            int actualIndex = LinearSearch.Search<Person>(data, new Person() { Age = 66, Name = "stupidExample" }, Comparer<Person>.Create(stupidExample));
+            int expectedIndex = 1;
+
+            Assert.AreEqual(expectedIndex, actualIndex, "Binary search did not provide the correct index with a custom comparer");
+        }
+
+        private class Person
+        {
+            public int Age { get; set; }
+            public string Name { get; set; }
+        }
+    }
+}
