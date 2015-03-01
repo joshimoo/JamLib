0adcdd273c71dd907fefa1272f11ca75bfbe162d
Joshua Moody <joshimoo@hotmail.de>
2015-03-01 20:53:32 +0100

    Added the Crypto Namespace Implemented the Gronsfeld Cipher

---
+++ JamLib/Crypto/GronsfeldCipher.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+
+namespace JamLib.Crypto
+{
+    public class GronsfeldCipher
+    {
+        string alphabet = " !\"#$%&'()*+,-./0123456789:<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        Dictionary<char, int> map;
+
+        /// <summary>
+        /// Uses the Default alphabet: " !"#$%&'()*+,-./0123456789:<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+        /// </summary>
+        public GronsfeldCipher() { this.map = GenerateAlphabetMapping(alphabet); }
+
+        /// <summary>
+        /// Uses a user specified alphabet
+        /// </summary>
+        public GronsfeldCipher(string alphabet)
+        {
+            this.alphabet = alphabet;
+            this.map = GenerateAlphabetMapping(alphabet);
+        }
+
+        /// <summary>
+        /// TODO: the way I deal with the custom alphabet is kinda hmm :(
+        /// But this way I don't have to constantly lookup the char index in the custom alphabet
+        /// </summary>
+        private Dictionary<char, int> GenerateAlphabetMapping(string alphabet)
+        {
+            var map = new Dictionary<char, int>(alphabet.Length);
+            for (int i = 0; i < alphabet.Length; i++)
+            {
+                map.Add(alphabet[i], i);
+            }
+
+            return map;
+        }
+
+        /// <summary>
+        /// Encode a message
+        /// Each element for the key represents a rotation value (binary number)
+        /// The rotation value must be smaller then the alphabet length
+        /// </summary>
+        public char[] Encode(string msg, byte[] key) { return Transcode(msg, key, false); }
+
+        /// <summary>
+        /// Decode a message
+        /// Each element for the key represents a rotation value (binary number)
+        /// The rotation value must be smaller then the alphabet length
+        /// </summary>
+        public char[] Decode(string msg, byte[] key) { return Transcode(msg, key, true); }
+
+        /// <summary>
+        /// Takes care of the actuall de/encoding
+        /// TODO: add char[] overloads for msg
+        /// </summary>
+        private char[] Transcode(string msg, byte[] key, bool decode)
+        {
+            Contract.Requires(msg != null && Contract.ForAll(msg, c => map.ContainsKey(c)));
+            Contract.Requires(key != null && Contract.ForAll(key, k => k < alphabet.Length));
+
+            char[] output = new char[msg.Length];
+
+            for (int i = 0; i < msg.Length; i++)
+            {
+                output[i] = Shift(msg[i], key[i % key.Length], decode);
+            }
+
+            return output;
+        }
+
+        /// <summary>
+        /// Shifts a passed element inside of the user supplied alphabet
+        /// </summary>
+        private char Shift(char c, int rot, bool reverse)
+        {
+            int index = map[c];
+            int newIndex = reverse ? index - rot : index + rot;
+
+            // Wrapping the new Index, a rot value >= alphabet lenght makes no sense, so we only wrap once instead of continously
+            if (newIndex < 0) { newIndex += alphabet.Length; }
+            else if (newIndex >= alphabet.Length) { newIndex -= alphabet.Length; }
+            return alphabet[newIndex];
+        }
+    }
+}
---
+++ JamLibTests/Crypto/GronsfeldCipherTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace JamLib.Crypto.Tests
+{
+    [TestClass()]
+    public class GronsfeldCipherTests
+    {
+        [TestMethod()]
+        public void UserSuppliedAlphabet()
+        {
+            string alphabet = "ABCD0123.!, ";
+            byte[] key = new byte[] { 1, 2 };
+            string msg = "AAAA01"; // You can only use elements from the alphabet
+
+            var c = new GronsfeldCipher(alphabet);
+            Assert.AreEqual(msg, new String(c.Decode(new String(c.Encode(msg, key)), key)));
+            CollectionAssert.AreEqual(msg.ToCharArray(), c.Decode(new String(c.Encode(msg, key)), key));
+        }
+
+
+        [TestMethod()]
+        public void SymetryTest()
+        {
+            byte[] key = new byte[] { 3, 2, 5, 7, 1, 4, 0 };
+            string msg = "Hello World!";
+            var c = new GronsfeldCipher();
+
+            Assert.AreEqual(msg, new String(c.Decode(new String(c.Encode(msg, key)), key)));
+            CollectionAssert.AreEqual(msg.ToCharArray(), c.Decode(new String(c.Encode(msg, key)), key));
+        }
+
+        [TestMethod()]
+        public void EncodeTest()
+        {
+            var c = new GronsfeldCipher();
+            Assert.AreEqual("HYEMYDUMPS", new String(c.Encode("EXALTATION", new byte[] { 3, 1, 4, 1, 5 })));
+            Assert.AreEqual("M%muxi%dncpqftiix\"", new String(c.Encode("I love challenges!", new byte[] { 4, 5, 1, 6, 2 })));
+            Assert.AreEqual("Uix!&kotvx3", new String(c.Encode("Test input.", new byte[] { 1, 4, 5, 8, 6, 2, 1, 4 })));
+        }
+
+        [TestMethod()]
+        public void DecodeTest()
+        {
+            var c = new GronsfeldCipher();
+            Assert.AreEqual("EXALTATION", new String(c.Decode("HYEMYDUMPS", new byte[] { 3, 1, 4, 1, 5 })));
+            Assert.AreEqual("I love challenges!", new String(c.Decode("M%muxi%dncpqftiix\"", new byte[] { 4, 5, 1, 6, 2 })));
+            Assert.AreEqual("Test input.", new String(c.Decode("Uix!&kotvx3", new byte[] { 1, 4, 5, 8, 6, 2, 1, 4 })));
+        }
+    }
+}
